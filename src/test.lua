18172314ad12dfe1cb6dcb256606b0946827fd27
Martin Lund <martin.lund@keep-it-simple.com>
2014-09-09 21:37:47 +0200

    Updated test script

--- src/test.lua
+++ src/test.lua
@@ -1,12 +1,20 @@
-#!lua-5.2.3/src/lua
+#!lua-5.2.3/src/lua -i
 
 --
--- Test Gear lua test script
+-- Test Gear test script
 --
 
 -- Include Test Gear API
 require("testgear")
 
-device=connect("192.168.0.1")
-device.load("wifi")
-print(device.wifi)
+-- First test case (TODO):
+--  device = connect("127.0.0.1")
+--  device.load("shell")
+--  device.shell.command = "touch bla.txt"
+--  device.shell.command()
+--  device.unload("shell")
+--  disconnect(device)
+
+device = connect("127.0.0.1")
+device.load("shell")
+print(device.shell)
