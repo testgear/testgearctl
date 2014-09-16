ab1e58c6ed2e3103abc9ca61f843dc0079bbba31
Martin Lund <martin.lund@keep-it-simple.com>
2014-09-17 00:09:58 +0200

    Updated test.lua

--- src/test.lua
+++ src/test.lua
@@ -6,16 +6,28 @@
 -- ./testgearctl --interactive ./test.lua
 --
 
--- First test case (TODO):
--- device = connect("127.0.0.1")
--- device.load("dummy")
--- device.dummy.string0 = "Hello world!"
--- device.dummy.command0()
--- device.unload("dummy")
--- disconnect(device)
+-- Dummy test case
+if (true) then
+    device = connect("127.0.0.1")
+    device.list_plugins()
+    device.load("dummy")
+    device.dummy.char0 = 42
+    print(device.dummy.char0)
+    device.dummy.short0 = 4242
+    print(device.dummy.short0)
+    device.dummy.int0 = 424242
+    print(device.dummy.int0)
+    device.dummy.float0 = 42.42
+    print(device.dummy.float0)
+    device.dummy.string0 = "Hello world!"
+    print(device.dummy.string0)
+    device.dummy.command0()
+    device.unload("dummy")
+    disconnect(device)
+end
 
-device = connect("127.0.0.1")
-device.load("dummy")
+--device = connect("127.0.0.1")
+--device.load("dummy")
 
 --device.unload("dummy")
 --disconnect(device)
