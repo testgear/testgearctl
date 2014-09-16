a3eeed617350de2646250ccad6ec178b8d225a87
Martin Lund <martin.lund@keep-it-simple.com>
2014-09-16 17:59:21 +0200

    Added support to run commands
    This commit makes it possible to run commands from lua, eg.:
    device.shell.run()
    
    This commit also adds automatic expansion of variables and commands when loading a plugin.

--- src/testgear.lua
+++ src/testgear.lua
@@ -35,11 +35,22 @@ function connect (hostname)
         end,
 
         __readindex = function (t,k)
-            if (type(rawget(t,k)) ~= "function") then
-                print("read preexisting variable " .. k)
+            local result = 0
+            if (k ~= "_type") and (k ~= "_name") and (k ~= "_parent") then
+                if (type(rawget(t,k)) == "function") then
+                    print("read preexisting function " .. k)
+                    local name = t._name .. "." .. k
+                    local handle = t._parent._handle
+                    result = tg_run(handle, name)
+--                    print(result)
+--                    rawset(t,k,v)
+--                    print(result)
+                else
+                    print("read preexisting variable " .. k)
+                end
             end
             return rawget(t,k)
-        end,
+        end
     }
 
     print("Connected to " .. hostname)
@@ -47,6 +58,14 @@ function connect (hostname)
 
     function self.load (name)
         local status
+        local properties
+
+        function string:split(sep)
+            local sep, fields = sep or ":", {}
+            local pattern = string.format("([^%s]+)", sep)
+            self:gsub(pattern, function(c) fields[#fields+1] = c end)
+            return fields
+        end
 
         if (type(name) ~= "string") then
             print("Error: Plugin name must be a string")
@@ -63,14 +82,47 @@ function connect (hostname)
 
         -- Add property table
         self[name] = {}
-        -- Watch property events
-        setmetatable(self[name], self)
+
+        -- Add type lookup table
+        self[name]._type = {}
+
+        -- Save parent
+        self[name]._parent = self
+
+        -- Save name of table
+        self[name]._name = name
 
         -- Populate property table with plugin vars/functions
---        self[name].command = "touch bla.txt"
---        self[name].run_command = function ()
---            print("Running " .. self[name].command)
---        end
+        properties = tg_plugin_list_properties(self._handle, name)
+        local property = properties:split(",")
+        for i = 1, #property do
+            word = property[i]:split(":")
+            if (word[2] == "0") then
+                self[name][word[1]] = 0
+                self[name]._type[word[1]] = "char"
+            elseif (word[2] == "1") then
+                self[name][word[1]] = 0
+                self[name]._type[word[1]] = "short"
+            elseif (word[2] == "2") then
+                self[name][word[1]] = 0
+                self[name]._type[word[1]] = "int"
+            elseif (word[2] == "3") then
+                self[name][word[1]] = 0
+                self[name]._type[word[1]] = "float"
+            elseif (word[2] == "4") then
+                self[name][word[1]] = ""
+                self[name]._type[word[1]] = "string"
+            elseif (word[2] == "5") then
+                self[name][word[1]] = ""
+                self[name]._type[word[1]] = "data"
+            elseif (word[2] == "6") then
+                self[name][word[1]] = function () end
+                self[name]._type[word[1]] = "command"
+            end
+        end
+
+        -- Watch property events
+        setmetatable(self[name], self)
     end
 
     function self.unload (name)
@@ -80,7 +132,7 @@ function connect (hostname)
             return -1
         end
 
-        status = tg_unload(_handle, name)
+        status = tg_unload(self._handle, name)
         if (status < 0) then
             print("Error: " .. tg_error())
             return -1
@@ -94,7 +146,7 @@ function connect (hostname)
 
     function self.list_plugins()
         local plugins
-        plugins = tg_list_plugins(_handle)
+        plugins = tg_list_plugins(self._handle)
         print("Available plugins: " .. plugins)
     end
 
