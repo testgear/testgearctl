6e9472e1455375301b39479e2c37c52cb8f1e547
Martin Lund <martin.lund@keep-it-simple.com>
2014-09-16 17:55:25 +0200

    Updated test.lua

--- src/test.lua
+++ src/test.lua
@@ -2,11 +2,14 @@
 -- Test Gear test script
 --
 
+-- To run test script simply do:
+-- ./testgearctl --interactive ./test.lua
+--
+
 -- First test case (TODO):
 --  device = connect("127.0.0.1")
 --  device.load("shell")
 --  device.shell.command = "touch bla.txt"
---  print(device.shell.command)
 --  device.shell.run_command()
 --  device.unload("shell")
 --  disconnect(device)
