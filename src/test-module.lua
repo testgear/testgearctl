1f66fff842272d479a30aedf3d35f9af595b387b
Martin Lund <martin.lund@keep-it-simple.com>
2014-09-16 21:51:54 +0200

    Completed lua test-module.lua
    The test-module.lua now includes test of all API functions in the Test
    Gear lua modue.

--- src/test-module.lua
+++ src/test-module.lua
@@ -15,27 +15,61 @@ device = tg_connect("127.0.0.1")
 plugins = tg_list_plugins(device)
 print("Avaiable plugins: " .. plugins)
 
--- Load shell plugin
-result = tg_load(device, "shell")
+-- Load dummy plugin
+result = tg_load(device, "dummy")
 print(result)
+
 
 -- List properties for plugin
-properties = tg_plugin_list_properties(device, "shell")
-print("Properties for shell: " .. properties)
+properties = tg_plugin_list_properties(device, "dummy")
+print("Plugin properties: " .. properties)
 
 -- Query general plugin information
-version = tg_get_string(device, "shell.version")
+version = tg_get_string(device, "dummy.version")
 print("Plugin version: " .. version)
 
--- Set command
-tg_set_string(device, "shell.command", "touch blabla.txt")
+-- Set char
+tg_set_char(device, "dummy.char0", 42)
+
+-- Get char
+char0 = tg_get_char(device, "dummy.char0")
+print("char0 = " .. char0)
+
+-- Set short
+tg_set_short(device, "dummy.short0", 4242)
+
+-- Get short
+short0 = tg_get_short(device, "dummy.short0")
+print("short0 = " .. short0)
+
+-- Set int
+tg_set_int(device, "dummy.int0", 424242)
+
+-- Get int
+int0 = tg_get_int(device, "dummy.int0")
+print("int0 = " .. int0)
+
+-- Set float
+tg_set_float(device, "dummy.float0", 42.42)
+
+-- Get float
+float0 = tg_get_float(device, "dummy.float0")
+print("float0 = " .. float0)
+
+-- Set string
+tg_set_string(device, "dummy.string0", "Hello world!")
+
+-- Get string
+string0 = tg_get_string(device, "dummy.string0")
+print("string0 = " .. string0)
 
 -- Run command
-result = tg_run(device, "shell.run")
+result = tg_run(device, "dummy.command0")
 print(result)
 
--- Unload shell plugin
-result = tg_unload(device, "shell")
+
+-- Unload dummy plugin
+result = tg_unload(device, "dummy")
 print(result)
 
 -- Disconnect from Test Gear server
