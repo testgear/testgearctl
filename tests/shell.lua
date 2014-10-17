f949e2dd1f4d49559dcc83f71b72753a00e6a189
Martin Lund <martin.lund@keep-it-simple.com>
2014-10-17 21:22:05 +0200

    Fixed return value for function (command) calls

--- tests/shell.lua
+++ tests/shell.lua
@@ -7,7 +7,8 @@ device = connect("tcp://127.0.0.1:8000")
 device.load("shell")
 -- tg
 device.shell.command = "touch bla.txt"
-device.shell.run()
+status = device.shell.run()
+if (status ~= 0) then fail() end
 -- tg
 device.unload("shell")
 disconnect(device)
