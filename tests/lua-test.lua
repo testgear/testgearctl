8921b08498f8b522a02d54e9f8d60f31841041b9
Martin Lund <martin.lund@keep-it-simple.com>
2014-10-17 19:25:16 +0200

    lua-test.lua: Added more test cases

--- tests/lua-test.lua
+++ tests/lua-test.lua
@@ -5,7 +5,6 @@
 -- To run this test script simply do (in src/ dir):
 -- ./testgearctl ../tests/lua-tests.lua
 --
-
 -- tg
 -- Print stuff to screen
 print("GREETINGS PROFESSOR FALKEN")
@@ -20,4 +19,11 @@ until answer=="y" or answer=="n"
 if answer=="n" then fail() end
 -- tg
 -- Force lua error
-- 
+-
+-- tg
+-- Wait for user input to continue
+io.write("Press <Enter> to continue...")
+io.read()
+-- tg
+-- Force Test Gear failure
+fail()
